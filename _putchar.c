478d855a191da40a95d1e7222b449aabd40e690e
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-26 04:10:46 -0700

    Initialization

---
+++ _putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - main block, write char to stdout
+ * @c: character
+ * Return: 1 on success, -1 on failure
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
